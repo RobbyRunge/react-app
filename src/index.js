8ceeee1483a5553090c9e2011b707e2edd5d0680
RobbyR98 <robbyrunge@yahoo.com>
2025-08-04 20:18:09 +0200

    Add Bootstrap and update Navbar component styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Navbar/>);
