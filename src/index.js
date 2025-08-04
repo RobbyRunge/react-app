a6c97b198033caf48bdf29938ddcc05ba78cd09c
RobbyR98 <robbyrunge@yahoo.com>
2025-08-04 20:11:59 +0200

    Add Navbar component and update index.js to render it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-// Importiere React, um JSX verwenden zu können
 import React from "react";
-
-// Importiere die neue ReactDOM-API aus "react-dom/client"
 import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar";
 
-// Erstelle ein einfaches JSX-Element, das eine Überschrift darstellt
-const element = <h1>Hello World</h1>;
-
-// Initialisiere das Root-Element und rendere das JSX-Element in das DOM
-ReactDOM.createRoot(document.getElementById('root')).render(element);
+ReactDOM.createRoot(document.getElementById('root')).render(<Navbar/>);
