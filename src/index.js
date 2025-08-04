998906830fbb5a583657486ea7b175926f98d7a2
RobbyR98 <robbyrunge@yahoo.com>
2025-08-04 20:26:17 +0200

    Add Product component with basic card layout and styling

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Product from "./components/product";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Navbar/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Product/>);
