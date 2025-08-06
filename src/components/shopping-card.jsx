6700ada3edfba07bad994252466b5ea715895b5c
RobbyR98 <robbyrunge@yahoo.com>
2025-08-06 21:09:12 +0200

    Add initial items to ShoppingCard component state for rendering cart contents

--- src/components/shopping-card.jsx
+++ src/components/shopping-card.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
- 
+
 class ShoppingCard extends Component {
-  state = {  } 
-  render() { 
+  state = {
+    items: [{
+      amount: 1,
+      name: 'Tomaten',
+      price: 4.9
+    },
+    {
+      amount: 1,
+      name: 'Äpfel',
+      price: 7.9
+    },
+    {
+      amount: 1,
+      name: 'Gurken',
+      price: 3.9
+    },
+    {
+      amount: 1,
+      name: 'Birnen',
+      price: 5.9
+    }]
+  }
+  render() {
     return <div className='shopping-card'>
       <h2>Warenkorb</h2>
+      {this.state.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.price}€</div>)}
     </div>;
   }
- }
-  
- export default ShoppingCard;
+}
+
+export default ShoppingCard;
