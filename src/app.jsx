6da62f18467bd72b5c2f3557e1be3057d4560cb3
RobbyR98 <robbyrunge@yahoo.com>
2025-08-07 20:34:54 +0200

    Refactor ShoppingCard and Product components to manage cart items dynamically

--- src/app.jsx
+++ src/app.jsx
@@ -4,18 +4,33 @@ import Product from "./components/product";
 import ShoppingCard from "./components/shopping-card";
 
 class App extends Component {
-  state = {}
+  state = {
+    items: []
+  }
+
+  addItem = (amount, name, price) => {
+    let currentItems = this.state.items;
+    currentItems.push({
+      amount, 
+      name, 
+      price
+    })
+    this.setState({items: currentItems})
+    console.log(this.state);
+    
+  }
+
   render() {
     return <React.Fragment>
       <Navbar />
       <div className="main-container">
         <div className="product-container">
-          <Product img="carrots.jpg" title="Tomaten" description="Füge Tomaten zu deinen Warenkorb hinzu" />
-          <Product img="gurke.jpg" title="Gurken" description="Füge Gurken zu deinen Warenkorb hinzu" />
-          <Product img="äpfel.jpg" title="Äpfel" description="Füge Äpfel zu deinen Warenkorb hinzu" />
-          <Product img="birne.jpg" title="Birnen" description="Füge Birnen zu deinen Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Tomaten', 4.99)} img="carrots.jpg" title="Tomaten" description="Füge Tomaten zu deinen Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Gurken', 3.99)} img="gurke.jpg" title="Gurken" description="Füge Gurken zu deinen Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Äpfel', 7.99)} img="äpfel.jpg" title="Äpfel" description="Füge Äpfel zu deinen Warenkorb hinzu" />
+          <Product onAdd={() => this.addItem(1, 'Birnen', 5.99)} img="birne.jpg" title="Birnen" description="Füge Birnen zu deinen Warenkorb hinzu" />
         </div>
-        <ShoppingCard />
+        <ShoppingCard items={this.state.items}/>
       </div>
     </React.Fragment>;
   }
--- src/components/shopping-card.jsx
+++ src/components/shopping-card.jsx
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
 
 class ShoppingCard extends Component {
-  state = {
-    items: [{
-      amount: 1,
-      name: 'Tomaten',
-      price: 4.9
-    },
-    {
-      amount: 1,
-      name: 'Äpfel',
-      price: 7.9
-    },
-    {
-      amount: 1,
-      name: 'Gurken',
-      price: 3.9
-    },
-    {
-      amount: 1,
-      name: 'Birnen',
-      price: 5.9
-    }]
-  }
+  state = { }
   render() {
     return <div className='shopping-card'>
       <h2>Warenkorb</h2>
-      {this.state.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.price}€</div>)}
+      {this.props.items.map(item => <div key={item.name}>{item.amount}x {item.name} {item.price}€</div>)}
     </div>;
   }
 }
